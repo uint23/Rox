65bb473a0067e913ca22d5ddb6b696c2cc4eb5a5
uint <unsigned@netcat.uk>
2026-06-28 00:16:46 +0100

    drawing crosshair

--- source/main.cc
+++ source/main.cc
@@ -2,15 +2,44 @@
 
 #include <raylib.h>
 
+void draw_ui(void);
+void draw_3d(void);
+void draw_crosshair(void);
+
 Camera3D camera;
+
+void draw_ui(void)
+{
+	draw_crosshair();
+}
+
+void draw_3d(void)
+{
+	BeginMode3D(camera);
+
+	DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY);
+	DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);
+	DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);
+	DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);
+
+	EndMode3D();
+}
+
+void draw_crosshair(void)
+{
+	int radius = 4;
+	int cx = GetScreenWidth() / 2;
+	int cy = GetScreenHeight() / 2;
+	DrawCircle(cx-radius, cy-radius, 4, WHITE);
+}
 
 int main(void)
 {
-	InitWindow(640, 480, "Rhythm Game");
+	InitWindow(640, 480, "Rox - Aim Trainer");
 	SetTargetFPS(144);
 	DisableCursor();
 
-	(Camera3D) camera = {
+	camera = {
 		.position = (Vector3){ 0.0f, 2.0f, 4.0f },
 		.target = (Vector3){ 0.0f, 2.0f, 0.0f },
 		.up = (Vector3){ 0.0f, 1.0f, 0.0f },
@@ -35,14 +64,10 @@ int main(void)
 
 		BeginDrawing();
 		ClearBackground(WHITE);
-		BeginMode3D(camera);
 
-                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY);
-		DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);
-		DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);
-		DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);
+		draw_3d();
+		draw_ui();
 
-		EndMode3D();
 		EndDrawing();
 	}
 	CloseWindow();
